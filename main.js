5f2deb519b71e1abcacbcada400dc40c65c32f3d
Mahdi Dibaiee <mdibaiee@aol.com>
2015-11-02 15:00:15 +0330

    chore: upgrade to babel 6

---
+++ main.js
@@ -0,0 +1,8 @@
+export default bot => {
+  // read configurations from bot.data, users set bot.data in initialize.j.defaults
+  let data = bot.data.template;
+
+  // add a help record for your plugin's commands
+  bot.help('call', 'calls someone', 'call <name>');
+}
+
