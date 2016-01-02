5825aee54fca50eec23c043f8095b9cf9513402a
Mahdi Dibaiee <mdibaiee@aol.com>
2016-01-02 15:50:24 +0330

    chore(ghooks): setup ghooks to run grunt before commit

--- build/index.js
+++ build/index.js
@@ -1,13 +1,15 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-exports.default = function (bot) {
-  // read configurations from bot.data, users set bot.data in initialize.j.defaults
+exports['default'] = function (bot) {
+  // read configurations from bot.data, users set bot.data in initialize.js
   var data = bot.data.template;
 
   // add a help record for your plugin's commands
-  bot.help('call', 'calls someone', 'call <name>');
+  bot.help('example', 'shows an example', 'example <name>');
 };
+
+module.exports = exports['default'];
