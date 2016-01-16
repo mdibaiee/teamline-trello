177bd71373d29829ef4f3bcf79843e2a5fde83e2
Mahdi Dibaiee <mdibaiee@aol.com>
2016-01-16 18:23:31 +0330

    fix: use `bot.config` instead of `bot.data` fix: explain `.help` chore: configure ghooks to run grunt on pre-commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 export default bot => {
-  // read configurations from bot.data, users set bot.data in initialize.js
-  let data = bot.data.template;
+  // read configurations from bot.config, users set bot.config in `config.js`
+  let config = bot.config.template;
 
-  // add a help record for your plugin's commands
+  // Add a help record for your plugin's commands
+	// When the user issues `help`, there will be a list of command names along
+	// with their short description (second argument).
+	// If the user issues `help` with a command name, like `help example`,
+	// the long description (last argument) will be shown.
   bot.help('example', 'shows an example', 'example <name>');
 }
