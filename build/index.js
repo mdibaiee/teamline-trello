315007bd2ed6cde977cf9282ca85a1b2a696ad08
Mahdi Dibaiee <mdibaiee@aol.com>
2016-01-17 09:16:29 +0330

    chore: remove my name from LICENSE

--- build/index.js
+++ build/index.js
@@ -5,10 +5,14 @@ Object.defineProperty(exports, '__esModule', {
 });
 
 exports['default'] = function (bot) {
-  // read configurations from bot.data, users set bot.data in initialize.js
-  var data = bot.data.template;
+  // read configurations from bot.config, users set bot.config in `config.js`
+  var config = bot.config.template;
 
-  // add a help record for your plugin's commands
+  // Add a help record for your plugin's commands
+  // When the user issues `help`, there will be a list of command names along
+  // with their short description (second argument).
+  // If the user issues `help` with a command name, like `help example`,
+  // the long description (last argument) will be shown.
   bot.help('example', 'shows an example', 'example <name>');
 };
 
