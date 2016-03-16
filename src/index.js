578336cf50c1d8b791b9e84b4651feef5c573e48
Mahdi Dibaiee <mdibaiee@aol.com>
2016-03-16 10:51:32 +0330

    fix(index): wrap the whole thing in a try/catch block to catch errors

--- src/index.js
+++ src/index.js
@@ -8,56 +8,60 @@ const DEFAULTS = {
   lists: ['todo', 'doing', 'done', 'homeless']
 };
 export default async (server, db, config = {}) => {
-  const { sequelize, Sequelize } = db;
+  try {
+    const { sequelize, Sequelize } = db;
 
-  const APP = config.sync.trello.app;
-  const USER = config.sync.trello.user;
+    const APP = config.sync.trello.app;
+    const USER = config.sync.trello.user;
 
-  if (!APP || !USER) {
-    throw new Error('Please set sync.trello.app and user.');
+    if (!APP || !USER) {
+      throw new Error('Please set sync.trello.app and user.');
+    }
+
+    const trello = new Trello(APP, USER);
+    const { get } = request(trello);
+
+    config.trello = Object.assign({
+      user: await get('/1/members/me')
+    }, DEFAULTS);
+
+    const { Company, Team, Employee, Project } = db.sequelize.models; // eslint-disable-line
+
+    sequelize.define('Trello', {
+      trelloId: Sequelize.STRING,
+      modelId: Sequelize.STRING,
+      type: Sequelize.ENUM('team', 'employee', 'project', 'role') // eslint-disable-line
+    });
+
+    await db.sequelize.sync();
+
+    let syncing = false;
+
+    const resync = async () => {
+      if (syncing) return;
+      syncing = true;
+      try {
+        await sync(trello, db, config);
+      } catch (e) {
+        console.error('Trello synchronisation error', e, e.stack);
+      }
+      try {
+        await commentActions(trello, db, config);
+      } catch (e) {
+        console.error('Trello commenting error', e, e.stack);
+      }
+      syncing = false;
+    };
+
+    server.on('refresh', resync);
+
+    resync();
+
+    hooks(trello, server, db, config);
+
+    // const INTERVAL = 10000;
+    // setInterval(resync, INTERVAL);
+  } catch (e) {
+    console.error(e);
   }
-
-  const trello = new Trello(APP, USER);
-  const { get } = request(trello);
-
-  config.trello = Object.assign({
-    user: await get('/1/members/me')
-  }, DEFAULTS);
-
-  const { Company, Team, Employee, Project } = db.sequelize.models; // eslint-disable-line
-
-  sequelize.define('Trello', {
-    trelloId: Sequelize.STRING,
-    modelId: Sequelize.STRING,
-    type: Sequelize.ENUM('team', 'employee', 'project', 'role') // eslint-disable-line
-  });
-
-  await db.sequelize.sync();
-
-  let syncing = false;
-
-  const resync = async () => {
-    if (syncing) return;
-    syncing = true;
-    try {
-      await sync(trello, db, config);
-    } catch (e) {
-      console.error('Trello synchronisation error', e, e.stack);
-    }
-    try {
-      await commentActions(trello, db, config);
-    } catch (e) {
-      console.error('Trello commenting error', e, e.stack);
-    }
-    syncing = false;
-  };
-
-  server.on('refresh', resync);
-
-  resync();
-
-  hooks(trello, server, db, config);
-
-  // const INTERVAL = 10000;
-  // setInterval(resync, INTERVAL);
 };
