bbe2abfc68fe8be878df8b4853409c240b9d841c
Mahdi Dibaiee <mdibaiee@aol.com>
2016-06-08 12:32:02 +0430

    fix(sync): accept owner's name with @

--- src/sync.js
+++ src/sync.js
@@ -247,7 +247,7 @@ export default async (trello, db, config) => {
 
           const match = /owner:\s*(\w+)/i.exec(card.desc);
           if (match && match[1]) {
-            const username = match[1].trim();
+            const username = match[1].trim().replace('@', '');
             const emp = await Employee.findOne({
               where: {
                 username,
